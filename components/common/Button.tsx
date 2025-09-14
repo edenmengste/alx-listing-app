6f50e8e6eedfe2d440cdb3332f6ed056c9acc75e
edenmengste <edenmengste@gmail.com>
2025-09-14 21:02:12 +0400

    added files in assets

---
+++ components/common/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button: React.FC = () => {
+  return (
+    <button className="px-4 py-2 font-bold text-white bg-blue-500 rounded-md hover:bg-blue-700">
+      Click Me
+    </button>
+  );
+};
+
+export default Button;
---
+++ components/common/Card.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Card: React.FC = () => {
+  return (
+    <div className="p-4 border border-gray-200 rounded-md shadow-sm">
+      {/* This is a placeholder for the Card component */}
+    </div>
+  );
+};
+
+export default Card;
